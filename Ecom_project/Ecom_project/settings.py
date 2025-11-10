c1f2816147f707e0648e5d5afada179812fbed3c
Sandeep <sandeeprokkala123@gmail.com>
2025-11-10 19:39:07 +0530

    changed in settings

--- Ecom_project/Ecom_project/settings.py
+++ Ecom_project/Ecom_project/settings.py
@@ -136,12 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URL = 'static/'
 STATICFILES_DIRS=['static/']
 
 # whitenoise static stuff
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 
