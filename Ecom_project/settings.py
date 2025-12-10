14a9df76465f5f391d255ac13c46008912adbc14
Sandeep <sandeeprokkala123@gmail.com>
2025-12-10 12:00:09 +0530

    fixed env12

--- Ecom_project/settings.py
+++ Ecom_project/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vy92&$#@bg^dc-%cne9ozveutir9ctz+o0i^p0ekhtrz7q@_p^'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -162,12 +162,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SITE_URL = "http://127.0.0.1:8000"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "sandeeprokkala66@gmail.com"
-EMAIL_HOST_PASSWORD = 'rdam bmmz qxfk scyf'
 
 LOGIN_REDIRECT_URL='Home'
 LOGOUT_REDIRECT_URL='Home'
